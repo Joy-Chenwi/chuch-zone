0d61ec03628bd81148dd3e907fa7f4f4ded5ab1b
Joy-Chenwi <coderj652@gmail.com>
2025-05-13 15:49:06 -0400

    almost done with the homepage

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 const router = require("express-router");
 const bodyParser = require("body-parser");
 
+let isRegistered = true;
+
 const auth = require("./logic/auth.js");
 
 app.use(bodyParser.json());
@@ -14,17 +16,16 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    console.log("this shit works");
-    res.render("register");
+    let isRegistered = true;
+    res.render("index");
 })
 
+app.get("/signup", (req, res) => {
+    res.render("register");
+});
 app.get("/login", (req, res) => {
     res.render("login");
 });
-
-app.get("/char", (req, res) => {
-    res.send("<p> hello </p>");
-})
 
 app.listen(3010, () => {
     console.log("Everything is running right");
